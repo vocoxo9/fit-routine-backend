9503c6e81918a225f46c2bfd2f94232b25d815f7
Tokemo <similes_pax_9h@icloud.com>
2025-06-05 13:54:34 +0900

    잘못된 mapper 폴더 수정, MemberMapper 위치 수정 및 병합

--- src/main/java/kr/co/khedu/fitroutine/member/model/dao/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package kr.co.khedu.fitroutine.member.model.dao;
-
-import kr.co.khedu.fitroutine.member.model.dto.MemberProfile;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MemberMapper {
-    MemberProfile getMemberProfile(int memberId);
-}
