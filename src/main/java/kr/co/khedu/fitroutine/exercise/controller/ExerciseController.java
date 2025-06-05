296eaef0eaf951570590413f217858f9b9692d58
Tokemo <similes_pax_9h@icloud.com>
2025-06-05 12:44:00 +0900

    Refactor ExerciseController

--- src/main/java/kr/co/khedu/fitroutine/exercise/controller/ExerciseController.java
+++ src/main/java/kr/co/khedu/fitroutine/exercise/controller/ExerciseController.java
@@ -2,30 +2,22 @@ package kr.co.khedu.fitroutine.exercise.controller;
 
 import kr.co.khedu.fitroutine.exercise.model.vo.ExerciseOpenData;
 import kr.co.khedu.fitroutine.exercise.service.ExerciseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
-@RequestMapping("/exercise")
+@RequestMapping("/exercises")
 public final class ExerciseController {
-
-
     private final ExerciseService exerciseService;
-    @Autowired
+
     public ExerciseController(ExerciseService exerciseService) {
         this.exerciseService = exerciseService;
     }
 
-    /**
-     * @return 운동 공공데이터 리스트 반환
-     */
-    @GetMapping("/openData")
+    @GetMapping("/open-data")
     public ResponseEntity<List<? extends ExerciseOpenData>> getAllExerciseOpenDataList() {
-        List<? extends ExerciseOpenData> exerciseList = exerciseService.getAllExerciseOpenDataList();
-        return ResponseEntity.ok(exerciseList);
+        return ResponseEntity.ok(exerciseService.getAllExerciseOpenDataList());
     }
 }
