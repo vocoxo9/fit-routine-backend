b51139f8e4412fc9e018d25ff7d16c1811a88a00
seongjae213 <vhrvkflrur@naver.com>
2025-06-05 09:47:17 +0900

    Refactor BoardService

--- src/main/java/kr/co/khedu/fitroutine/board/service/BoardService.java
+++ src/main/java/kr/co/khedu/fitroutine/board/service/BoardService.java
@@ -1,9 +1,20 @@
 package kr.co.khedu.fitroutine.board.service;
 
+import kr.co.khedu.fitroutine.board.model.dao.BoardMapper;
 import kr.co.khedu.fitroutine.board.model.dto.PopularBoardDTO;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface BoardService {
-    List<? extends PopularBoardDTO> getPopularBoardTop3();
+@Service
+public final class BoardService {
+    private final BoardMapper boardMapper;
+
+    public BoardService(BoardMapper boardMapper) {
+        this.boardMapper = boardMapper;
+    }
+
+    public List<? extends PopularBoardDTO> getPopularBoardTop3() {
+        return boardMapper.getPopularBoardTop3();
+    }
 }
