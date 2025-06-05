7167efe351bc5ea1c654ca480cb61c5b6d7799cd
Tokemo <similes_pax_9h@icloud.com>
2025-06-05 13:56:37 +0900

    Refactor MemberProfile

--- src/main/java/kr/co/khedu/fitroutine/member/model/dto/MemberProfile.java
+++ src/main/java/kr/co/khedu/fitroutine/member/model/dto/MemberProfile.java
@@ -5,21 +5,30 @@ import lombok.Getter;
 
 @Getter
 public final class MemberProfile {
-    private final int memberId;
+    private final long memberId;
     private final String email;
     private final String nickname;
-    private final String birth;
+    private final String birthAt;
     private final String phone;
     private final char gender;
     private final int height;
     private final int weight;
 
     @Builder
-    public MemberProfile(int memberId, String email, String nickname, String birth, String phone, char gender, int height, int weight) {
+    private MemberProfile(
+            long memberId,
+            String email,
+            String nickname,
+            String birthAt,
+            String phone,
+            char gender,
+            int height,
+            int weight
+    ) {
         this.memberId = memberId;
         this.email = email;
         this.nickname = nickname;
-        this.birth = birth;
+        this.birthAt = birthAt;
         this.phone = phone;
         this.gender = gender;
         this.height = height;
