cdd8a96828e62ac756521dbcbdd6c253c887c775
Tokemo <similes_pax_9h@icloud.com>
2025-06-07 21:56:26 +0900

    Update MemberMapper

--- src/main/java/kr/co/khedu/fitroutine/member/mapper/MemberMapper.java
+++ src/main/java/kr/co/khedu/fitroutine/member/mapper/MemberMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-    Member findMemberById(long memberId);
+    Member findMemberByBlogId(long blogId);
 
     MemberProfile getMemberProfile(long memberId);
 }
