9625d8ebf49f40484af0bdd99500efa2aa50b3b4
Tokemo <similes_pax_9h@icloud.com>
2025-06-07 22:44:25 +0900

    Reformat BoardController

--- src/main/java/kr/co/khedu/fitroutine/board/controller/BoardController.java
+++ src/main/java/kr/co/khedu/fitroutine/board/controller/BoardController.java
@@ -3,7 +3,6 @@ package kr.co.khedu.fitroutine.board.controller;
 import kr.co.khedu.fitroutine.board.model.dto.PopularBoard;
 import kr.co.khedu.fitroutine.board.service.BoardService;
 import kr.co.khedu.fitroutine.board.utils.ImageFileUtil;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,22 +29,25 @@ public final class BoardController {
             @RequestPart("title") String title,
             @RequestPart("category") String category,
             @RequestPart("content") String content,
-            @RequestPart(value = "images", required = true) MultipartFile[] images
+            @RequestPart(value = "images") MultipartFile[] images
     ) {
+        // 추후 토큰에서 추출하도록 변경
         int memberId = Integer.parseInt(token);
 
-
         long boardId = boardService.saveBoardDetail(title, category, content, memberId);
-
         if (boardId < 0) {
             return ResponseEntity.status(400).body("failure");
         }
 
         for (MultipartFile image : images) {
-            String changedFileName = ImageFileUtil.saveFile(image); // 서버에 파일 저장
+            String changedFileName = ImageFileUtil.saveFile(image);
+
             boolean result = boardService.saveBoardImage(image.getOriginalFilename(), changedFileName, boardId);
-            if(!result){ return ResponseEntity.status(400).body("failure");}
+            if (!result) {
+                return ResponseEntity.status(400).body("failure");
+            }
         }
+
         return ResponseEntity.status(200).body("success");
     }
 
