7614cd2892d75550caa5f5c6a6388cee0f9a8acb
Tokemo <similes_pax_9h@icloud.com>
2025-06-09 00:05:00 +0900

    반환 값이 null일 수 있는 매퍼 메서드들에 @Nullable 어노테이션 추가 및 리펙터링

--- src/main/java/kr/co/khedu/fitroutine/member/mapper/MemberMapper.java
+++ src/main/java/kr/co/khedu/fitroutine/member/mapper/MemberMapper.java
@@ -1,5 +1,6 @@
 package kr.co.khedu.fitroutine.member.mapper;
 
+import jakarta.annotation.Nullable;
 import kr.co.khedu.fitroutine.member.model.dto.MemberEditInfo;
 import kr.co.khedu.fitroutine.member.model.dto.MemberProfile;
 import kr.co.khedu.fitroutine.member.model.vo.Member;
@@ -7,9 +8,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
-    Member findMemberByBlogId(long blogId);
+    @Nullable Member findMemberByBlogId(long blogId);
 
-    MemberProfile getMemberProfile(long memberId);
+    @Nullable MemberProfile getMemberProfile(long memberId);
 
     int editMemberProfile(MemberEditInfo memberEditInfo);
 
