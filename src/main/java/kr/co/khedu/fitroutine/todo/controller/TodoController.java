700c5af723e69f41392008d000d52dc63efabcc6
Kim-ilhyeon <kih3915@naver.com>
2025-06-14 02:12:42 +0900

    임의 memberId값을 token값으로 수정

--- src/main/java/kr/co/khedu/fitroutine/todo/controller/TodoController.java
+++ src/main/java/kr/co/khedu/fitroutine/todo/controller/TodoController.java
@@ -1,7 +1,9 @@
 package kr.co.khedu.fitroutine.todo.controller;
 
+import kr.co.khedu.fitroutine.security.model.dto.UserDetailsImpl;
 import kr.co.khedu.fitroutine.todo.service.TodoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,9 @@ public final class TodoController {
     }
 
     @GetMapping("/routine-rankings/monthly/me")
-    public ResponseEntity<?> getRoutineMvpMyRank() {
-        long memberId = 1;
-        return ResponseEntity.ok(todoService.getgetRoutineMvpMyRank(memberId));
+    public ResponseEntity<?> getRoutineMvpMyRank(
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        return ResponseEntity.ok(todoService.getgetRoutineMvpMyRank(userDetails.getMemberId()));
     }
 }
