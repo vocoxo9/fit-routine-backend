960f8d91434175280a550d60c5a56afc2f5dd01a
Tokemo <similes_pax_9h@icloud.com>
2025-06-14 21:08:36 +0900

    MemberController를 open class로 변경

--- src/main/java/kr/co/khedu/fitroutine/member/controller/MemberController.java
+++ src/main/java/kr/co/khedu/fitroutine/member/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/members")
 @Validated
-public final class MemberController {
+public class MemberController {
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
