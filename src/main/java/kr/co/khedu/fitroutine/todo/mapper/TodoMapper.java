0420c23a9ad89b46c81e6d35b1c6c2c3a089401b
Kim-ilhyeon <kih3915@naver.com>
2025-06-14 14:24:24 +0900

    Update TodoMapper.java

--- src/main/java/kr/co/khedu/fitroutine/todo/mapper/TodoMapper.java
+++ src/main/java/kr/co/khedu/fitroutine/todo/mapper/TodoMapper.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Mapper
 public interface TodoMapper {
-    @Nullable List<? extends RoutineMvpTOP3> getRoutineMvpTOP3();
+    List<? extends RoutineMvpTOP3> getRoutineMvpTOP3();
 
     @Nullable MyRank getRoutineMvpMyRank(long memberId);
 }
